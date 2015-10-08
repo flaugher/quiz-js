5b2655cfb60785918545f4aad7b4fe37e01e45db
Robert Flaugher <rlfprog@gmail.com>
2015-10-07 21:18:18 -0700

    Replace alert dialogs that prompt user with HTML sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
-
 var quiz = {
-    "name": "Football player quiz",
-    "description": "How many football players' teams can you name?",
-    "question_start": "What team did ",
-    "question_end": " play on?",
+    "name":"Super Hero Name Quiz",
+    "description":"How many super heroes can you name?",
+    "question":"What is the real name of ",
     "questions": [
-        { "question": "Bart Starr", "answer": "Green Bay Packers" },
-        { "question": "Tom Brady", "answer": "New England Patriots"},
-        { "question": "Dick Butkus", "answer": "Chicago Bears"}
+        { "question": "Superman", "answer": "Clark Kent" },
+        { "question": "Batman", "answer": "Bruce Wayne" },
+        { "question": "Wonder Woman", "answer": "Dianna Prince" }
     ]
-};
+}
 
-var score = 0;  // Initialize score
+/// DOM references start with "$" ///
+var $question = document.getElementById("question");
+var $score = document.getElementById("score");
+var $feedback = document.getElementById("feedback");
+
+/// view functions ///
+function update(element, content, klass) {
+    var p = element.firstChild || document.createElement("p");
+    p.textContent = content;
+    element.appendChild(p);
+    if (klass) {
+        p.className = klass;
+    }
+}
 
 // Because of hoisting, we can call the 'play' function before defining it
 play(quiz);
 
 function play(quiz) {
-    welcome();
+    // welcome();
+    var score = 0;  // Initialize score
+    update($score, score);
+
     // Main game loop
     for (var i = 0, question, answer, max = quiz.questions.length; i < max; i++) {
         question = quiz.questions[i].question;
@@ -27,24 +41,28 @@ function play(quiz) {
     gameOver();
 
     // Nest these next auxilary functions so they can access play's variables
+    /*
     function welcome() {
         alert("Welcome to our football quiz!");
     }
+    */
 
-    function ask(questionquestion_) {
-        return prompt(quiz.question_start + question + quiz.question_end)
+    function ask(question) {
+        update($question, quiz.question + question);
+        return prompt("Enter your answer: ");
     }
 
     function check(answer) {
         if (answer === quiz.questions[i].answer) {
-            alert("Correct!");
+            update($feedback, "Correct!", "right");
             score++;
+            update($score, score);
         } else {
-            alert("Sorry, that's not the right answer.");
+            update($feedback, "Wrong!", "wrong");
         }
     }
 
     function gameOver() {
-        alert("The game is over.  You scored " + score + " points.");
+        update($question, "Game over, you scored " + score + " points");
     }
 }
